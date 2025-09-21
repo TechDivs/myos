69862e6eece99a14f76b04b5dbae32e4a2580bb9
Divyansh Khatikar <divyanshkhatikar@gmail.com>
2025-09-21 11:37:02 +0000

    Added String Functions and Kernel Panic

--- common.c
+++ common.c
@@ -26,7 +26,7 @@ void printf(const char *fmt, ...) {
                 }
                 case 'd': { // Print an integer in decimal.
                     int value = va_arg(vargs, int);
-                    unsigned magnitude = value; // https://github.com/nuta/operating-system-in-1000-lines/issues/64
+                    unsigned magnitude = value; 
                     if (value < 0) {
                         putchar('-');
                         magnitude = -magnitude;
@@ -61,4 +61,42 @@ void printf(const char *fmt, ...) {
 
 end:
     va_end(vargs);
+}
+
+
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
